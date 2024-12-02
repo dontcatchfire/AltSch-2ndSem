5686981685b32edcb2845da7d06433dbf8ff6e93
Onyinye Ibiam <155055823+dontcatchfire@users.noreply.github.com>
2024-12-02 19:29:00 +0100

    wrote few lines of bash

---
+++ onyinye.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+who_am_i() {
+    echo "Hello! Onyinye [REDACTED] Ibiam, here 👋. Your new favourite Cloud Padawan."
+}
+
+bio() {
+    
+}
+
+years_in_tech() {
+    echo "It's been about a year now - jumping all the way in. But I don't know, does watching Tech YouTube content make you someone who has been in tech for a while? I highly doubt it does."
+}
+
+dream_job() {
+    echo "CyberSec Professional at Facebook (i know), BHIS, or Ring."
+}
+
+goals() {
+    
+}
