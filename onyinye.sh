d5e88ad05bbeb8464974479639a1a47fe0cef436
Onyinye Ibiam <155055823+dontcatchfire@users.noreply.github.com>
2024-12-03 04:46:34 +0100

    completed bash file assignment

--- onyinye.sh
+++ onyinye.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 who_am_i() {
-    echo "Hello! Onyinye [REDACTED] Ibiam, here 👋. Your new favourite Cloud Padawan."
+    echo "Hello! Onyinye [REDACTED] Ibiam, here 👋. Your new favourite Cloud Padawan. hehe."
 }
 
 bio() {
-    
+    echo "I’m what you would call a tech enthusiast. I have a fascination for the cyberspace, the mega network called the Internet, and different weird worlds that form as a result of machines being able to talk to each other and shake hands. I actually have a background in Microbiology. Maybe I would be involved some marraige (or situationship) that could happen between Microbiology and IT in some way. Maybe."
 }
 
 years_in_tech() {
@@ -13,9 +13,23 @@ years_in_tech() {
 }
 
 dream_job() {
-    echo "CyberSec Professional at Facebook (i know), BHIS, or Ring."
+    echo "CyberSec Professional at Facebook (i know), BHIS, Ring, Netflix..."
 }
 
 goals() {
-    
+    echo "Get hired for an InfoSec or a CyberSec role - cos that's where the action is at!"
+    echo "Explore all or most of tech the much I can, cos it's huuuuge - like an infinity pizza."
+    echo "Kickstart and maintain a formidable reading habit and culture - I want to escape and distance myself from brain rot; it's brutal out there."
+    echo "Learn Python programming - I hear it's useful."
+    echo "Make my own Jarvis - oh, boy!"
+    echo "Do cool stuff, make cool stuff - YOLO (do people still do YOLO?)"
+    echo "Write more and consistently - helps with bannishing brain rot, too."
+}
+
+hobbies() {
+    echo "Fantasizing about watching a movie, but never getting the chance to - I think I like this little life."
+    echo "Browsing the web and poking around until it leads me to a page that seems out of place - I'm probably joking here."
+    echo "Writing. I like it and I hate it. My brain doesn't kickstart when I need it to, sometimes."
+    echo "Having pleasant and productive conversations with ChatGPT - my AI compadre"
+    echo "Trying not to dox myself on social media - so help me G*d! Bruh..."
 }
